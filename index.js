1aa6be90652209f06a46b7ecbfc369a620279d38
jatinkumar0 <76422129+jatinkumar0@users.noreply.github.com>
2022-12-06 13:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Jagdeesh ! How are You ?')
+  response.send('Hello Manish ! How are You Doing ?')
 })
 
 app.listen(app.get('port'), function() {
